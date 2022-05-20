9d5f9c0af82f28b623c0e0800d760ccc199b0cf8
mahbub099 <mahbubrmn99@gmail.com>
2022-05-20 10:32:04 +0600

    add css page

--- lms/lms/urls.py
+++ lms/lms/urls.py
@@ -19,5 +19,7 @@ from . import views
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('', views.teacher),
+    path('', views.adminpage),
+    path('teacher/', views.teacher),
+    path('student/', views.student),
 ]
--- lms/lms/views.py
+++ lms/lms/views.py
@@ -2,5 +2,11 @@ from django.shortcuts import render
 from .models import Post
 
 
+def adminpage(request):
+    return render(request, 'lms/adminpage.html',)
+
 def teacher(request):
     return render(request, 'lms/teacher.html',)
+
+def student(request):
+    return render(request, 'lms/student.html',)
