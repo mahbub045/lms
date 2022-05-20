03a358506e3eff617fac6efa5c488cf5fc86060e
mahbub099 <mahbubrmn99@gmail.com>
2022-05-20 09:53:22 +0600

    fixed bug

--- lms/lms/views.py
+++ lms/lms/views.py
@@ -2,6 +2,5 @@ from django.shortcuts import render
 from .models import Post
 
 
-def home(request):
-	all_post = Post.objects.all().order_by('-data_posted')
-    return render(request, 'lms/index.html', {'all' : all_post})
+def teacher(request):
+    return render(request, 'lms/teacher.html',)
